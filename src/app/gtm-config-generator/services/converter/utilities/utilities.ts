30444ed32f112cbcbacae4e55b79f7514b244fad
Guan-Xin <alex0972112871@gmail.com>
2023-09-04 08:58:58 +0800

    fix: remove excel code comments Remove the comment inside the object to be able to parse the spec as the JSON format.

--- src/app/gtm-config-generator/services/converter/utilities/utilities.ts
+++ src/app/gtm-config-generator/services/converter/utilities/utilities.ts
@@ -136,6 +136,24 @@ export function fixJsonString(inputString: string) {
   try {
     let fixedString = inputString;
 
+    // Remove multi-line comments first (/**/)
+    fixedString = fixedString.replace(/\/\*[\s\S]*?\*\//gm, '');
+
+    // Remove single-line comments that appear after JSON values
+    let lines = fixedString.split('\n');
+    for (let i = 0; i < lines.length; i++) {
+      let commentIndex = lines[i].indexOf('//');
+      if (commentIndex >= 0) {
+        let precedingChars = lines[i].substring(0, commentIndex);
+        if (/[:,]\s*$/.test(precedingChars)) {
+          // Comment appears after a JSON value, remove only the comment
+          lines[i] = lines[i].substring(0, commentIndex);
+        }
+      }
+    }
+
+    fixedString = lines.join('\n');
+
     // Replace single quotes with double quotes
     fixedString = fixedString.replace(/'/g, '"');
 
