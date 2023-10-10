2ab4768df56ff9a02f11cb4c2481ae2a828c453c
Guan-Xin <alex0972112871@gmail.com>
2023-10-10 23:00:18 +0800

    remove router modules for performance.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,12 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { appRoutes } from './app.routes';
 import { SharedModule } from './shared.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(), // required
     provideHttpClient(), // required
-    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), // if routers are used
     importProvidersFrom(SharedModule),
   ],
 };
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainPageComponent } from './components/main-page/main-page.component';
-
-export const appRoutes: Routes = [
-  {
-    path: '',
-    component: MainPageComponent,
-  },
-];
